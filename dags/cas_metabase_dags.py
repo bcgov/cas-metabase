4ad0913666989eb0978c1254714fb7276f719d32
Dylan Leard <dylan@button.is>
2021-09-23 12:12:25 -0700

    chore: add dag to handle prod-test restore

--- dags/cas_metabase_dags.py
+++ dags/cas_metabase_dags.py
@@ -46,3 +46,33 @@ metabase_full_backup_dag = DAG('walg_backup_metabase_full', default_args=default
 
 create_backup_task(metabase_full_backup_dag,
                    namespace, 'cas-metabase-patroni')
+
+"""
+###############################################################################
+#                                                                             #
+# DAG to restore data from PROD (if in test)                                  #
+#                                                                             #
+###############################################################################
+"""
+
+"""
+If we're in the test namespace, dag first restores data from prod and then fixes the db connection passwords to be for the test db.
+"""
+prod_test_restore_dag = DAG('cas_metabase_prod_test_restore', schedule_interval=None,
+                    default_args=default_args)
+
+def prod_test_restore(dag):
+    return PythonOperator(
+        python_callable=trigger_k8s_cronjob,
+        task_id='metabase-prod-test-restore',
+        op_args=['cas-metabase-prod-test-restore', namespace],
+        dag=dag)
+
+def fix_db_pass(dag):
+    return PythonOperator(
+        python_callable=trigger_k8s_cronjob,
+        task_id='metabase-db-pass',
+        op_args=['cas-metabase-db-pass', namespace],
+        dag=dag)
+
+prod_test_restore(prod_test_restore_dag) >> fix_db_pass(prod_test_restore_dag)
