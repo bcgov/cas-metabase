a388bcf99e789812abae3c8eaa5b8dcf306c1b5c
Josh Gamache <jsgamache@gmail.com>
2025-09-09 12:11:13 -0600

    refactor: update imports to use new split-out package

--- dags/cas_metabase_dags.py
+++ dags/cas_metabase_dags.py
@@ -6,7 +6,7 @@ Triggering the cas-metabase-acme-renewal cron job
 from dag_configuration import default_dag_args
 from trigger_k8s_cronjob import trigger_k8s_cronjob
 from walg_backups import create_backup_task
-from airflow.operators.python_operator import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from airflow import DAG
 import os
